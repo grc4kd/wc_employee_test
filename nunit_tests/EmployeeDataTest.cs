1116722949ea0f94078e5dc841bc360f1d824285
Griffin Crane <grc4kd@users.noreply.github.com>
2022-02-06 11:50:10 -0600

    Removed boilerplate code / ambiguous endpoint. Fixed test setup code in EmployeeDataTest.

--- nunit_tests/EmployeeDataTest.cs
+++ nunit_tests/EmployeeDataTest.cs
@@ -19,7 +19,7 @@ namespace nunit_tests
                                             .UseInMemoryDatabase("employeeDB")
                                             .Options;
 
-            _context = new ApiContext(options);
+            var _context = new ApiContext(options);
             var _seeder = new ApiContextSeeder(_context);
             _seeder.SeedData();
             return _context;
